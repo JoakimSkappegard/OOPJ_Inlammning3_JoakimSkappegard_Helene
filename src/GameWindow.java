a4b44d58ae970364edd391d94306009c974eef19
Helenes-Hub <hrydros@gmail.com>
2024-11-01 11:46:28 +0100

    Hamster genereras(alltid) winchecker tillagd

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -14,7 +14,6 @@ public class GameWindow implements ActionListener {
     JLabel label_textfield=new JLabel();
     JButton[] game_buttons=new JButton[16];
     JButton[] option_buttons=new JButton[3];
-    private Path p1 = Paths.get("src/bilder/hamster/");
 
 
     GameWindow() {
@@ -24,7 +23,6 @@ public class GameWindow implements ActionListener {
         frame.getContentPane().setBackground(new Color(50, 50, 50));
         frame.setLayout(new BorderLayout());
         frame.setVisible(true);
-
 
         //fixar till bordern som syns högst upp, läggs på panel_options längre ner i koden
         label_textfield.setBackground(new Color(200, 200, 200));
@@ -47,6 +45,8 @@ public class GameWindow implements ActionListener {
         panel_buttons.setBackground(new Color(150, 150, 150));
         //
 
+
+        /*
         Icon[] bilder = new ImageIcon[15];
         for (int i = 0; i < bilder.length; i++) {
             String imagePath = "src/bilder/hamster/" + (i + 1) + ".png";
@@ -57,25 +57,27 @@ public class GameWindow implements ActionListener {
                 System.out.println(e.getMessage());
             }
         }
-
+        */
 
         //
-
-
+        String[] buttonNames = new String[game_buttons.length];
+/*
         for (int i = 0; i < game_buttons.length; i++) {
             game_buttons[i] = new JButton();
             panel_buttons.add(game_buttons[i]);
             //bestämmer att alla knappar förutom den sista ska få en siffra
-            if (i!=game_buttons.length-1) {
+            if (i!=game_buttons.length-1 && bilder[i] != null) {
                 game_buttons[i].setIcon(bilder[i]);
                 // game_buttons[i].setText(String.valueOf(i+1));
             }
+            game_buttons[i].setName(String.valueOf(i+1));
             game_buttons[i].setFocusable(false);
             game_buttons[i].setFont(new Font("Times New Roman", Font.BOLD, 120));
             game_buttons[i].addActionListener(this);
+
         }
 
-
+ */
 
         //lägger på options texten och knapparna på panel_options
         panel_options.add(label_textfield);
@@ -83,13 +85,13 @@ public class GameWindow implements ActionListener {
             panel_options.add(optionButton);
         }
 
-
         frame.add(panel_options, BorderLayout.NORTH);
         frame.add(panel_buttons);
-
+        panel_buttons.revalidate();
+        panel_buttons.repaint();
+        frame.revalidate();
+        frame.repaint();
     }
-
-
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -100,8 +102,19 @@ public class GameWindow implements ActionListener {
         int buttonY=buttonY(button);
         button.setLocation(blankX, blankY);
         game_buttons[15].setLocation(buttonX,buttonY);
-
     }
+
+    public boolean winChecker(){
+        for(int i=0;i<game_buttons.length;i++){
+            String buttonName = game_buttons[i].getName();
+
+            if (!buttonName.equals(String.valueOf(i + 1))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 
     public int blankY (JButton b){
         float currentY= b.getAlignmentY();
